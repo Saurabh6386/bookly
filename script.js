36bae4e28aa911ca98633ac9def901a7a93735ad
Saurabh <sraj638637@gmail.com>
2023-10-13 15:51:13 +0530

    push to github

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+let cards = document.querySelector(".cards");
+function createUI() {
+  blogs.books.forEach((element) => {
+    console.log(element);
+    let li = document.createElement("li");
+    let image = document.createElement("img");
+    image.src = element.image;
+    console.log(image);
+    let title = document.createElement("h6");
+    title.innerText = element.title;
+    let author = document.createElement("p");
+    author.innerText = element.author;
+    let btn = document.createElement("button");
+    btn.innerText = "Buy now";
+    li.append(image, title, author, btn);
+    cards.append(li);
+  });
+}
+createUI();
